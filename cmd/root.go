767d63ce0c0d51721a68a6d44eee26c7b0979252
indium114 <156162907+indium114@users.noreply.github.com>
2026-03-28 18:26:35 +0200

    feat: command

--- cmd/root.go
+++ cmd/root.go
@@ -2,27 +2,36 @@ package cmd
 
 import (
 	"context"
-	"os"
+
+	"github.com/indium114/sorta/internal"
 
 	"github.com/charmbracelet/fang"
+	"github.com/charmbracelet/log"
 	"github.com/spf13/cobra"
 )
 
-// rootCmd represents the base command when called without any subcommands
-var rootCmd = &cobra.Command{
-	Use:   "sorta",
-	Short: "A simple CLI tool to sort files based on MIME type",
-	Run: func(cmd *cobra.Command, args []string) {
+var dir string
 
+var rootCmd = &cobra.Command{
+	Use:   "sorta [directory]",
+	Short: "A simple CLI tool to sort files based on MIME type",
+	Args:  cobra.MaximumNArgs(1),
+
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) > 0 {
+			dir = args[0]
+		}
+
+		if dir == "" {
+			dir = "."
+		}
+
+		internal.RunSorter(dir)
 	},
 }
 
 func Execute() {
-	err := fang.Execute(context.Background(), rootCmd)
-	if err != nil {
-		os.Exit(1)
+	if err := fang.Execute(context.Background(), rootCmd); err != nil {
+		log.Fatal(err)
 	}
-}
-
-func init() {
 }
